0c141c93002dd8ceb949d677a3efcf4f6ab5377b
Abdoul03 <samabdoul03@gmail.com>
2024-09-21 00:12:29 +0000

    add homescreen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class HomeScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Bonjour </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeScreen;

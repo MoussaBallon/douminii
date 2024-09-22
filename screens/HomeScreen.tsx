6a93ec66ba344ffd60c91ed8bf935fc654afaaab
Abdoul03 <samabdoul03@gmail.com>
2024-09-22 00:18:35 +0000

    tab_bar_okay

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class HomeScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Bonjour </Text>
-            </View>
-        );
-    }
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text> Bonjour</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({})
 
 export default HomeScreen;
--- screens/RestauranteScreen.tsx
+++ screens/RestauranteScreen.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 class RestauranteScreen extends Component {
     render() {
         return (
             <View>
-                
+                <Text>Resto</Text>
             </View>
         );
     }
